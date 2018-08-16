5b5ea506fdfff6a0f18e30361f04688ef4296c11
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-16 02:58:01 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(given_string)
+  
+  split_string_array = given_string.split(" ")
+  message=""
+  split_string_array.collect do |words|
+   message+="#{words.reverse} " 
+  end
+  message.strip
+end
+
