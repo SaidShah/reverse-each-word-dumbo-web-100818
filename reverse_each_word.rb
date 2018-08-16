832a6366043f552c112e2b69a01952c1a4a801cd
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-16 02:56:37 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(given_string)
   split_string_array = given_string.split(" ")
   message=""
   split_string_array.collect do |words|
-   message+="#{words.reverse} " 
+   message+="#{words.reverse}" 
   end
   message
 end
