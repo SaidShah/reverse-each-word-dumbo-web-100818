85b3fb65e639f927f68b0a79f0e258e871b1267d
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-16 02:53:51 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,8 @@
 def reverse_each_word(given_string)
   split_string_array = given_string.split(" ")
-split_string_array.collect do |words|
-    words.reverse.join(" ") 
+  split_string_array.collect do |words|
+    words.reverse.join(" ")*" " 
   end
   
 end
 
-def reverse_words(given_array)
-  
-end
