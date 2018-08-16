cff1b0c1f839289a1bfe3527be7a3b90521004f0
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-16 02:54:53 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(given_string)
   split_string_array = given_string.split(" ")
   
   split_string_array.collect do |words|
-    words.reverse*" " 
+    words.reverse
   end
   
 end
